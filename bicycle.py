251ae0e782aa2435a7b4df15a17fae8bdc5a4cb5
feeneyp <patrick@r3dpixel.com>
2015-02-03 15:38:44 +0000

    added frames and wheels classes

--- bicycle.py
+++ bicycle.py
@@ -1,22 +1,59 @@
 class Bicycle(object):
-  def __init__(self, model, weight, cost):
+  def __init__(self, frame_model, wheel_type): #frame models are 'City', 'Mountainous', 'Speedster', 'Kiddy', 'SuperBMX', 'Roadie'
+    self.frame_model = frame_model
+    self.wheel_type = wheel_type
+    
+class Frame(object):
+  def __init__(self, model="m", weight="w", cost="c"): #frame models are 'City', 'Mountainous', 'Speedster', 'Kiddy', 'SuperBMX', 'Roadie'
     self.model = model
     self.weight = weight
     self.cost = cost
     
+class Wheel(object):
+  def __init__(self, material, weight, cost): #wheel materials are alu, carbon, steel
+    self.material = material
+    self.weight = weight
+    self.cost = cost   
+    
 class Shop(object):
   def __init__(self):
-    self.bikes = {"Speedster":[25,100,11],"Mountainous":[25,200,12],"SuperBMX":[25,300,13],"Kiddy":[25,400,14],"Roadie":[25,500,15],"City":[25,600,16]}
-    self.inventory = {}
-    #bikes is a dict that stores data for model, weight, cost, and number available bikes
-    #inventory is a dict that the shop will use to list just the model of the bike and the number of them in stock
-    self.customer_list = [] #customer_list is a list of customer objects
+    self.frames = {
+      "Speedster":{"weight":25, "cost":100, "stock":11},
+      "Mountainous":{"weight":25, "cost":200, "stock":12},
+      "SuperBMX":{"weight":25, "cost":300, "stock":13},
+      "Kiddy":{"weight":25, "cost":400, "stock":14},
+      "Roadie":{"weight":25, "cost":500, "stock":15},
+      "City":{"weight":25, "cost":600, "stock":16}
+    }
+    #frames and wheels are dictionaries that stores data for model, weight, cost, and number available bikes
+    self.wheels = {
+      "alu":{"weight":25, "cost":100, "stock":11},
+      "carbon":{"weight":25, "cost":200, "stock":12},
+      "steel":{"weight":25, "cost":300, "stock":13}
+    }
+    #inventory is a dictionary of dictionaries that the shop will use to list 
+    #just the model or name of the item and the number of them in stock
+    self.inventory = {"frames":{},"wheels":{}}
+    #customer_list is a list of customer objects
+    self.customer_list = []
     
-  def stock_bikes (self):  #replaces the values of the items in the inventory (so far just list of strings) with Bicycle objects
-    for k,v in self.bikes.iteritems():
-      self.bikes[k] = Bicycle(k,v[0],v[1]) #bikes dict items are now Bicycle objects with model, weight, cost
-      self.inventory[k] = v[2] #inventory is a dict with k as str and v as int (num of bikes in stock)
-      #the line above populates the inventory dict which was an empty dict created in _init_
+    
+  def stock_frames_wheels (self):
+    #replaces the weight/cost info with Frame objects objects 
+    #and creates inventory dict with only stock info
+    for frame_name,v in self.frames.iteritems(): 
+      self.frames[frame_name] = Frame(model=frame_name, weight=v["weight"], cost=v["cost"]) 
+      self.inventory["frames"][frame_name] = v["stock"]
+    #replaces the weight/cost info with Frame Wheel objects 
+    #and creates inventory dict with only stock info
+    for k,v in self.wheels.iteritems():
+      self.wheels[k] = Wheel(frame_name,v["weight"],v["cost"])
+      self.inventory["wheels"][k] = v["stock"]   
+    
+    
+  def calc_cost_bike (self, frame_model, wheel_type):
+    cost = self.wheels[wheel_type].cost*2 + self.frames[frame_model].cost
+    return cost
   
   def bikes_in_budget (self,budget): #the shop object has a fct that returns a dict list of bike objects
     bikes_in_budget = {}
@@ -24,8 +61,7 @@ class Shop(object):
         if bike.cost * 1.2 <= budget:
           bikes_in_budget[bike.model] = bike
     return bikes_in_budget
-      
-    
+         
 class Customer(object):
   def __init__(self, budget):
     self.name = ""
@@ -33,10 +69,10 @@ class Customer(object):
     self.bikes_owned = []
     self.money_left = budget
   
-  def purchase_bike(self, shop, bicycle): #pass in a shop object and a bicycle object
-    self.bikes_owned.append(bicycle.model)
-    self.money_left = self.budget - (bicycle.cost * 1.2)
-    shop.inventory[bicycle.model] -= 1
-    #pseudo code for line above  shop.inventoryvalueforbikejustbought -= 1
-    #inventory of shop is a dict with k str and v int
+  def purchase_bike(self, shop, frame_model, wheel_type): #pass in a shop object and strings for frame and wheel type
+    self.bikes_owned.append(Bicycle(frame_model,wheel_type))
+    self.money_left = self.budget - (shop.calc_cost_bike(frame_model, wheel_type) * 1.2)
+    shop.inventory["frames"][frame_model] -= 1
+    shop.inventory["wheels"][wheel_type] -= 1
+
     
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from bicycle import Bicycle, Shop, Customer
+from bicycle import Bicycle, Frame, Wheel, Shop, Customer
 
 def main():
   bikesRUS = Shop()
   #print bikesRUS.bikes
-  bikesRUS.stock_bikes()
+  bikesRUS.stock_frames_wheels()
   #print bikesRUS.bikes
   print "this is the inventory {}\n".format(bikesRUS.inventory)
   customers = [Customer(200), Customer(500), Customer(1000)] #create three customers ad-hoc
@@ -12,16 +12,15 @@ def main():
     bikesRUS.customer_list.append(customer)
     customer.name = "customer"+str(i)
     i = i+1  
-  #print bikesRUS.customer_list 
-  for customer in bikesRUS.customer_list:
-    print "{} can afford: ".format(customer.name)
-    print "{}\n".format(bikesRUS.bikes_in_budget(customer.budget).keys())
-    bike_chosen = bikesRUS.bikes_in_budget(customer.budget).items()[0][1] #items() returns a list of items i.e. unordered list of k,v tuples and call to value() is necessary because you want the object part - k is the model name and v is a Bicycle object
-    customer.purchase_bike(bikesRUS,bike_chosen) #needs comment
+  print bikesRUS.customer_list 
+  customers[0].purchase_bike(bikesRUS,"Kiddy","carbon") #needs comment
+  customers[1].purchase_bike(bikesRUS,"Roadie","alu") #needs comment
+  customers[2].purchase_bike(bikesRUS,"Speedster","alu") #needs comment
   for customer in bikesRUS.customer_list:
     print "Bikes owned by {}: {}".format(customer.name,customer.bikes_owned)
     print "Budget left over for {}: {}\n".format(customer.name,customer.money_left)
-  print "this is the inventory after three customers purchased: {}\n".format(bikesRUS.inventory) 
+  print "this is the inventory after three customers purchased: {}\n".format(bikesRUS.inventory)
+
 
 if __name__ == "__main__":
   import random  #is this where I would put this??
